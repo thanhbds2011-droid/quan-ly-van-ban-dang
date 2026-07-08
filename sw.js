723c04abc339e3d9308d0448563eabe0285c0ba4
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-08 17:34:47 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,63 @@
+const CACHE_NAME = 'van-ban-dang-cache-v1.0.8';
 
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys
+          .filter(function(key) {
+            return key !== CACHE_NAME;
+          })
+          .map(function(key) {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  const requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin !== location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function(response) {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(function() {
+        return caches.match(event.request).then(function(cached) {
+          return cached || caches.match('./index.html');
+        });
+      })
+  );
+});
